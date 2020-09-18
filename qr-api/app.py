5074d7af577504650c82f9a32fe650cc7a7f4c5d
Ivan <ivanmp@live.com>
2020-09-17 19:20:07 -0600

    add endpoint to return all qrs

--- qr-api/app.py
+++ qr-api/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from bson import Binary, Code
+from bson.json_util import dumps, loads
+import json
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
+client = MongoClient()
 
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+class Connect(object):
+    @staticmethod
+    def get_connection():
+        return MongoClient()
+
+@app.route("/qrs", methods=['GET'])
+def getQRs():
+    db = client.test
+    cursor = db.qrs.find({})
+    return dumps(cursor)
+
+
+if __name__ == '__main__':
+    connection = Connect.get_connection()
+    app.run()
